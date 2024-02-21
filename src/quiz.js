175837022d34d601753fdb6e56f05ccb02b9b6c7
Henry Zhu <z5462946@ad.unsw.edu.au>
2024-02-21 15:08:33 +1100

    stub code for function adminQuizCreate

---
+++ src/quiz.js
@@ -0,0 +1,9 @@
+// Description:
+// Given basic details about a new quiz, create one for the logged in user.
+
+function adminQuizCreate(authUserId, name, description) {
+    return {
+        quizId: 2,
+    }
+  }
+  
