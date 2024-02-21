65e600631ab55ad2182a6b2b4e59b887c81455e6
Yanqi Yang <z5410578@ad.unsw.edu.au>
2024-02-22 10:45:26 +1100

    create adminUserDetailsUpdate

---
+++ src/auth.js
@@ -0,0 +1,8 @@
+//Description
+//Given an admin user's authUserId and a set of properties, update the properties of this logged in admin user.
+
+function adminUserDetailsUpdate(authUserId, email, nameFirst, nameLast) {
+    return {
+       
+    }
+}
