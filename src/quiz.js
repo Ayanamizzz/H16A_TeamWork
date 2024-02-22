b3e76177fd57d9a1613d6b5f5be41fb4adcd0cfd
Majin <z5437798@ad.unsw.edu.au>
2024-02-22 16:54:06 +1100

    create adminQuizList

---
+++ src/quiz.js
@@ -0,0 +1,11 @@
+// QuizList
+// Provide a list of all quizzes that are owned by the currently logged in user.
+function adminQuizList(authUserId) {
+    quizzes: [
+        {
+          quizId: 1,
+          name: 'My Quiz',
+        }
+    ]
+}
+  
