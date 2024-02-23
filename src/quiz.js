168e70e3e5b5ace0d8833b54617feb59ee100617
Henry Zhu <z5462946@ad.unsw.edu.au>
2024-02-23 21:00:53 +1100

    Update quiz.js

--- src/quiz.js
+++ src/quiz.js
@@ -1,43 +1,6 @@
-
-//stub for the adminQuizInfo
-//Get all of the relevant information about the current quiz.
-function adminQuizInfo (authUserId, quizId) {
-    return {
-        quizId: 1,
-        name: 'My Quiz',
-        timeCreated: 1683125870,
-        timeLastEdited: 1683125871,
-        description: 'This is my quiz',
-    }
-}
-
-//stub for the adminQuizNameUpdate
-//Update the name of the relevant quiz.
-function adminQuizNameUpdate(authUserId, quizId, name) {
-    return {}
-}
-
-=======
-// Description:
-// Given basic details about a new quiz, create one for the logged in user.
-
-function adminQuizCreate(authUserId, name, description) {
-    return {
-        quizId: 2,
-    }
-  }
-  
-// Description
-// Given a particular quiz, permanently remove the quiz.
-
-function adminQuizRemove(authUserId, quizId) {
-    return {
-    }
-}
-  
-
 // Description
 // Provide a list of all quizzes that are owned by the currently logged in user.
+
 function adminQuizList(authUserId) {
    return {
         quizzes: [
@@ -48,11 +11,38 @@ function adminQuizList(authUserId) {
         ]
    }
 }
+
+
+
+
+
+// Description:
+// Given basic details about a new quiz, create one for the logged in user.
+
+function adminQuizCreate(authUserId, name, description) {
+    return {
+        quizId: 2,
+    }
+}
   
 
-// Description
-// Get all of the relevant information about the current quiz.
 
+
+
+// Description
+// Given a particular quiz, permanently remove the quiz.
+
+function adminQuizRemove(authUserId, quizId) {
+    return {
+    }
+}
+  
+
+
+
+
+// Description:
+// Get all of the relevant information about the current quiz.
 
 function adminQuizInfo (authUserId, quizId) {
     return {
@@ -63,3 +53,27 @@ function adminQuizInfo (authUserId, quizId) {
         description: 'This is my quiz',
     }
 }
+
+
+
+
+
+
+// Description:
+// Update the name of the relevant quiz.
+
+function adminQuizNameUpdate(authUserId, quizId, name) {
+    return {}
+}
+
+
+
+
+
+// Description:
+// Update the description of the relevant quiz.
+
+function adminQuizDescriptionUpdate (authUserId, quizId, description) {
+    return {}
+}
+
