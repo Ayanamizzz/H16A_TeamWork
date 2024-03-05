4fe082f73cc57b6040dd0c17c124c9b68e3a1811
Henry Zhu <z5462946@ad.unsw.edu.au>
2024-03-05 21:33:52 +1100

    write test file for function adminQuizRemove

--- src/adminQuizRemove.test.js
+++ src/adminQuizRemove.test.js
@@ -1,19 +1,16 @@
-/* adminQuizCreate.test.js
+/* adminQuizRmove.test.js
 Error checking:
 1. AuthUserId is not a valid user.
-2. Name contains invalid characters. Valid characters are alphanumeric and spaces.
-3. Name is either less than 3 characters long or more than 30 characters long.
-4. Name is already used by the current logged in user for another quiz.
-5. Description is more than 100 characters in length (note: empty strings are OK).
+2. Quiz ID does not refer to a valid quiz.
+3. Quiz ID does not refer to a quiz that this user owns.
 */
 
 import { adminAuthRegister } from './auth.js'
 import { clear } from './other.js'
-import { adminQuizCreate } from './quiz.js'
+import { adminQuizCreate, adminQuizRmove } from './quiz.js'
 
 
-describe('courseCreate', () => {
-    // Tests for function adminQuizCreate \\
+describe('Tests for function adminQuizRmove', () => {
 
     // Test invaild input:
 
@@ -25,77 +22,43 @@ describe('courseCreate', () => {
 
         // Create quiz then check error message.
         const user = adminAuthRegister('linked@gmail.com', 'linked123456', 'Jack', 'Wang');
-        const result = adminQuizCreate(user.authUserId + 1, 'Quiz1', 'The first quiz');
-        
+        const quiz = adminQuizCreate(user.authUserId, 'Quiz1', 'The first quiz');
+
+        const result = adminQuizRmove(user.authUserId + 1, quiz.quizId);
         expect(result).toStrictEqual({ error: expect.any(String) });
     });
 
 
     test('Test invalid input', () => {
-        // Name contains invalid characters（Valid characters are alphanumeric and spaces.）
+        // Quiz ID does not refer to a valid quiz.
 
         // Reset before test.
         clear();
 
         // Create quiz then check error message.
         const user = adminAuthRegister('linked@gmail.com', 'linked123456', 'Jack', 'Wang');
-        const result = adminQuizCreate(user.authUserId, 'Qu!z@@', 'The first quiz');
+        const quiz = adminQuizCreate(user.authUserId, 'Quiz1', 'The first quiz');
+
+        const result = adminQuizRmove(user.authUserId, quiz.quizId + 1);
         expect(result).toStrictEqual({ error: expect.any(String) });
     });
 
-
     test('Test invalid input', () => {
-        // Name is either less than 3 characters long or more than 30 characters long.
-        // This test will check name is less than 3 characters long.
+        // Quiz ID does not refer to a quiz that this user owns.
 
         // Reset before test.
         clear();
 
         // Create quiz then check error message.
-        const user = adminAuthRegister('linked@gmail.com', 'linked123456', 'Jack', 'Wang');
-        const result = adminQuizCreate(user.authUserId, 'Q', 'The first quiz');
+        const user1 = adminAuthRegister('linked@gmail.com', 'linked123456', 'Jack', 'Wang');
+        const quiz1 = adminQuizCreate(user1.authUserId, 'Quiz1', 'The first quiz');
+
+        const user2 = adminAuthRegister('majin666@gmail.com', 'ziwhidnimnw', 'Ma', 'Jin');
+        const quiz2 = adminQuizCreate(user2.authUserId, 'Quiz1', 'The first quiz');
+
+        const result = adminQuizCreate(user1.authUserId, quiz2.quizId);
         expect(result).toStrictEqual({ error: expect.any(String) });
     });
-
-    test('Test invalid input', () => {
-        // Name is either less than 3 characters long or more than 30 characters long.
-        // This test will check name is more than 30 characters long.
-
-        // Reset before test.
-        clear();
-
-        // Create quiz then check error message.
-        const user = adminAuthRegister('linked@gmail.com', 'linked123456', 'Jack', 'Wang');
-        const result = adminQuizCreate(user.authUserId, 'Quiz for COMP1531 Group project Team Dream', 'The first quiz');
-        expect(result).toStrictEqual({ error: expect.any(String) });
-    });
-
-    test('Test invalid input', () => {
-        // Name is already used by the current logged in user for another quiz.
-
-        // Reset before test.
-        clear();
-
-        // Create quiz then check error message.
-        const user = adminAuthRegister('linked@gmail.com', 'linked123456', 'Jack', 'Wang');
-        const result1 = adminQuizCreate(user.authUserId, 'Quiz1', 'The first quiz');
-
-        const result2 = adminQuizCreate(user.authUserId, 'Quiz1', 'The first quiz');
-        expect(result2).toStrictEqual({ error: expect.any(String) });
-    });
-
-    test('Test invalid input', () => {
-        // Description is more than 100 characters in length (note: empty strings are OK).
-
-        // Reset before test.
-        clear();
-
-        // Create quiz then check error message.
-        const authUserId = adminAuthRegister('linked@gmail.com', 'linked123456', 'Jack', 'Wang');
-        const result = adminQuizCreate(authUserId, 'Quiz1', 'The first quiz that used for group project iteration 1 of course COMP1531 Software Engineering Fundamentals');
-        expect(result).toStrictEqual({ error: expect.any(String) });
-    });
-
 
 
     // Test successful adminQuizCreate:
@@ -108,8 +71,10 @@ describe('courseCreate', () => {
 
         // Create quiz that return quizId if no error
         const user = adminAuthRegister('linked@gmail.com', 'linked123456', 'Jack', 'Wang');
-        const result = adminQuizCreate(user.authUserId, 'Quiz1', 'The first quiz');
-        expect(result).toStrictEqual({ quizId: expect.any(Number) });
+        const quiz = adminQuizCreate(user.authUserId, 'Quiz1', 'The first quiz');
+
+        const result = adminQuizRmove(user.authUserId, quiz.quizId);
+        expect(result).toStrictEqual({ });
     });
 });
 
