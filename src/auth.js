fb6b5770e3ec49db324b607a02e0945e6d4517ec
Yanqi Yang <z5410578@ad.unsw.edu.au>
2024-03-07 18:13:41 +1100

    new branch redo the whole func to fix some issues

--- src/auth.js
+++ src/auth.js
@@ -1,3 +1,6 @@
+import { getData, setData } from './dataStore.js';
+import isEmail from 'validator/lib/isEmail';
+import { clear } from './others.js'
 // Description: 
 // Register a user with an email, password, and names, then returns their 
 // authUserId value.
@@ -45,15 +48,68 @@ function adminUserDetails(authUserId) {
 
 
 
-
-// Description:
-// Given an admin user's authUserId and a set of properties, update the properties of this logged in admin user.
+//Description
+//Given an admin user's authUserId and a set of properties, update the properties of this logged in admin user.
 
 function adminUserDetailsUpdate(authUserId, email, nameFirst, nameLast) {
-    return {
- 
+    let data = getData();
+    // Check whether the authUserId is a valid user
+    for (let i = 0; i < data.users.length; i++) {
+        if (data.users[i].authUserId === authUserId) {
+            // check whether the email has already been used by others
+            for (let i = 0; i < data.users.length; i++) {
+                if (data.users[i].authUserId !== authUserId && data.users[i].email === email) {
+                    return { error: 'Email is currently used by another user' };
+                }
+            }
+            // check whether email is satisfiy or not
+            if (isEmail(email, options) === false) {
+                return {
+                    error: 'Email does not satisfy',
+                }
+            };
+            // Check whether nameFirst is satisfy or not
+            for (let i = 0; i < nameFirst.length; i++) {
+                let letter = nameFirst.charCodeAt(i);
+                if (letter > 122 || (letter < 65 && letter != 32 && letter != 45 && letter != 39)) {
+                    return {
+                        error: 'nameFirst is not vaildNameFirst contains characters other than lowercase letters, uppercase letters, spaces, hyphens, or apostrophes.'
+                    }
+                }
+            }
+            // Check whether nameLast is satisfy or not
+            for (let i = 0; i < nameLast.length; i++) {
+                let letter = nameLast.charCodeAt(i);
+                if (letter > 122 || (letter < 65 && letter != 32 && letter != 45 && letter != 39)) {
+                    return {
+                        error: 'nameLast is not vaildNameLast contains characters other than lowercase letters, uppercase letters, spaces, hyphens, or apostrophes.'
+                    }
+                }
+            }
+            // Check whether the nameFirst.length satsify or not
+            if (nameFirst.length < 2 || nameFirst.length > 20) {
+                return {
+                    error: 'NameFirst is less than 2 characters or more than 20 characters.',
+                }
+            }
+
+            // Check whether the nameLast.lenght satisfy or not
+            if (nameLast.length < 2 || nameLast.length > 20) {
+                return {
+                    error: 'NameLast is less than 2 characters or more than 20 characters.',
+                }
+            }
+            setData(data);
+            data.users[i].email = email;
+            data.users[i].nameFirst = nameFirst;
+            data.users[i].nameLast = nameLast;
+            break;
+        }
     }
+    return {}
 }
+
+
 
 
 
@@ -73,3 +129,4 @@ function adminUserPasswordUpdate(authUserId, oldPassword, newPassword) {
 
 
 
+export { adminUserDetailsUpdate };
