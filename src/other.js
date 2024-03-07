716d1fe421a6baaffacc54e8a7cf99177966ba6e
Majin <z5437798@ad.unsw.edu.au>
2024-03-08 01:09:27 +1100

    commit fix

--- src/other.js
+++ src/other.js
@@ -1,18 +1,12 @@
 // Description:
 // Reset the state of the application back to the start.
 
-/*
- * @params {autherUserId} UserId
- * @returns {quizId} Id of quiz
- * 
-*/
 import { getData, setData } from "./dataStore.js";
 
 
 export function clear() {
     let data = getData();
     data.users = [];
-    //添加更多数据库里的内容
     data.quizzes = [];
     
     setData(data);
