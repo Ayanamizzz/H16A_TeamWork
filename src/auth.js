00d3302ad931e217697c92673ce87eb7b1dd9fbb
Yanqi Yang <z5410578@ad.unsw.edu.au>
2024-03-07 18:21:43 +1100

    new branch, redo the whole func to fix some issues

--- src/auth.js
+++ src/auth.js
@@ -1,3 +1,5 @@
+import { getData, setData } from './dataStore.js';
+import { clear } from './other.js';
 // Description: 
 // Register a user with an email, password, and names, then returns their 
 // authUserId value.
@@ -51,7 +53,7 @@ function adminUserDetails(authUserId) {
 
 function adminUserDetailsUpdate(authUserId, email, nameFirst, nameLast) {
     return {
- 
+
     }
 }
 
@@ -63,13 +65,60 @@ function adminUserDetailsUpdate(authUserId, email, nameFirst, nameLast) {
 // Given details relating to a password change, update the password of a logged in user.
 
 function adminUserPasswordUpdate(authUserId, oldPassword, newPassword) {
-    return {
-       
+    let data = getData();
+    // check whether the authUserId is a valid user
+    for (let i = 0; i < data.users.length; i++) {
+        if (data.users[i].authUserId === authUserId) {
+            // Check whether the oldPassword is the correct old password
+            if (data.users[i].password === oldPassword) {
+                // Check whether the newPassword has been used by others
+                for (let i = 0; i < data.users.length; i++) {
+                    if (data.users[i].password === newPassword) {
+                        return {
+                            error: 'Password is used by another user.'
+                        }
+                    }
+                }
+                // Check whether the newPassword.length satisfy or not
+                if (password.length < 8) {
+                    return {
+                        error: 'Password is less than 8 characters.',
+                    }
+                }
+                // Check whehther newPassword contain at least one number and at least one letter.
+                let Letter = 0;
+                let Number = 0;
+                for (let i = 0; i < password.length; i++) {
+                    const letter = password.charCodeAt(i);
+                    if ((letter >= 65 && letter <= 90) || (letter >= 97 && letter <= 122)) {
+                        Letter = 1;
+                    } else if (letter >= 48 && letter <= 57) {
+                        Number = 1;
+                    }
+                }
+                if (Letter === 0 || Number === 0) {
+                    return {
+                        error: 'Password does not contain at least one number and at least one letter.'
+                    }
+                }
+                // Update the password with newPassword
+                setData(data);
+                data.users[i].password = newPassword;
+                return {}
+
+            } else {
+                return { error: 'Old Password is not the correct old password.' }
+            }
+        }
+        return { error: 'AuthUserId is not a valid user.' }
     }
+
 }
 
 
 
 
 
+
+export { adminUserPasswordUpdate };
 
