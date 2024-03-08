f43980320791d852545664b369cdcab1f48f697c
Yuchao <z5413536@ad.unsw.edu.au>
2024-03-08 17:39:03 +1100

    adminQuizDescription function is completed , and passed all the test, waiting for merge

--- src/quiz.js
+++ src/quiz.js
@@ -1,79 +1,38 @@
-// Description
-// Provide a list of all quizzes that are owned by the currently logged in user.
+import { getData, setData } from './dataStore';
 
-function adminQuizList(authUserId) {
-   return {
-        quizzes: [
-            {
-                quizId: 1,
-                name: 'My Quiz',
-            }
-        ]
-   }
-}
-
-
-
-
-
-// Description:
-// Given basic details about a new quiz, create one for the logged in user.
-
-function adminQuizCreate(authUserId, name, description) {
-    return {
-        quizId: 2,
-    }
-}
-  
-
-
-
-
-// Description
-// Given a particular quiz, permanently remove the quiz.
-
-function adminQuizRemove(authUserId, quizId) {
-    return {
-    }
-}
-  
-
-
-
-
-// Description:
-// Get all of the relevant information about the current quiz.
-
-function adminQuizInfo (authUserId, quizId) {
-    return {
-        quizId: 1,
-        name: 'My Quiz',
-        timeCreated: 1683125870,
-        timeLastEdited: 1683125871,
-        description: 'This is my quiz',
-    }
-}
-
-
-
-
-
-
-// Description:
-// Update the name of the relevant quiz.
-
-function adminQuizNameUpdate(authUserId, quizId, name) {
-    return {}
-}
-
-
-
-
-
-// Description:
-// Update the description of the relevant quiz.
+/**
+ * Updates the description of a specific quiz.
+ * 
+ * @param {number} authUserId The ID of the user attempting to update the quiz description.
+ * @param {number} quizId The ID of the quiz whose description is being updated.
+ * @param {string} newDescription The new description for the quiz.
+ * @returns {Object}  empty object indicating the success of the operation or containing an error message.
+ */
 
 function adminQuizDescriptionUpdate (authUserId, quizId, description) {
-    return {}
+    const data = getData();
+    // Check if authUserId is a valid user
+    const userExists = data.users.some(user => user.authUserId === authUserId);
+    if (!userExists) {
+        return { error: 'AuthUserId is not a valid user.' };
+    }
+    // Find the quiz and check if it exists
+    const quiz = data.quizzes.find(quiz => quiz.quizId === quizId);
+    if (!quiz) {
+        return { error: 'Quiz ID does not refer to a valid quiz.' };
+    }
+    // Check if the quiz belongs to the user
+    if (quiz.ownerId !== authUserId) {
+        return { error: 'Quiz ID does not refer to a quiz that this user owns.' };
+    }    
+    // Check if the new description is valid
+    if (description.length > 100) {
+        return { error: 'Description is more than 100 characters in length (note: empty strings are OK).' };
+    }else {
+        quiz.description = description;
+        setData(data);
+        return { };
+    }
 }
 
+export { adminQuizDescriptionUpdate };
